156cf20b13cf5c68715e59ba147298dd13a1c41d
rohit231 <kumar231rohit@gmail.com>
2024-07-10 16:25:48 -0700

    React Form with Validation check

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import FormWithValidation from "./Components/FormWIthValidation";
+
+function App() {
+  return (
+    <>
+      <FormWithValidation />
+    </>
+  );
+}
+
+export default App;
